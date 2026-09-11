8d10d2a932402ab07ecc6005eedb3c6522e0e767
maksak_il <maksak_il@mail.ru>
2026-09-11 15:59:00 +0300

    Update LoginRequest.php

--- backend/app/Http/Requests/Auth/LoginRequest.php
+++ backend/app/Http/Requests/Auth/LoginRequest.php
@@ -8,11 +8,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class LoginRequest extends FormRequest
 {
-    public function authorize(): bool
-    {
-        return true;
-    }
-
     /**
      * @return array<string, array<int, string>>
      */
